142a1b6a17a9706a300fc7868307dcbdb5e24ccd
Rihab <rdhemaied@jems-group.com>
2023-09-05 10:45:56 +0200

    change sourcery version

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Swinject/Swinject.git", from: "2.4.0"),
-        .package(url: "https://github.com/krzysztofzablocki/Sourcery", from: "0.16.2"),
+        .package(url: "https://github.com/krzysztofzablocki/Sourcery", from: "2.0.2"),
         .package(url: "https://github.com/Quick/Nimble", from: "8.0.2"),
         .package(url: "https://github.com/Quick/Quick", from: "2.1.0")
     ],
