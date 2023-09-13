295c1510deaac03d4cc999d8e6a57f8a90b41762
Rihab <rdhemaied@jems-group.com>
2023-09-13 17:03:44 +0200

    support SPM

--- Package.swift
+++ Package.swift
@@ -6,16 +6,17 @@ import PackageDescription
 let package = Package(
     name: "AnnotationInject",
     platforms: [
-        .macOS(.v10_11)
+        .macOS(.v10_13)
     ],
     products: [
         .library(
             name: "AnnotationInject",
             targets: ["AnnotationInject"]),
+        .executable(name: "annotationinject-cli", targets: ["AnnotationCLI"])
     ],
     dependencies: [
         .package(url: "https://github.com/Swinject/Swinject.git", from: "2.4.0"),
-        .package(url: "https://github.com/krzysztofzablocki/Sourcery", from: "2.0.2"),
+        .package(url: "https://github.com/krzysztofzablocki/Sourcery", from: "1.8.0"),
         .package(url: "https://github.com/Quick/Nimble", from: "8.0.2"),
         .package(url: "https://github.com/Quick/Quick", from: "2.1.0")
     ],
@@ -24,12 +25,26 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "AnnotationInject",
-            dependencies: ["Swinject", "SourceryRuntime"],
-            path: "Sources"),
+            dependencies: [
+                "Swinject",
+                .product(name: "SourceryRuntime", package: "Sourcery")
+            ],
+            path: "Sources"
+        ),
         .testTarget(
             name: "AnnotationInjectTests",
             dependencies: ["AnnotationInject", "Quick", "Nimble"],
             path: "Tests"
+        ),
+        .target(
+            name: "AnnotationCLI",
+            dependencies: [],
+            path: "CLI",
+            resources: [
+                .copy("Templates"),
+                .copy("Scripts"),
+                .copy("Sources")
+            ]
         )
     ]
 )
